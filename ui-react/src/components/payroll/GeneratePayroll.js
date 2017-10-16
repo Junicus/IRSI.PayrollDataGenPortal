9207fc45440d6e699932bae779d33e2ae62a6a11
Nelson Segarra <junicus@outlook.com>
2017-10-16 16:01:58 -0400

    finished form ui for gen payroll, found the right datepicker

--- ui-react/src/components/payroll/GeneratePayroll.js
+++ ui-react/src/components/payroll/GeneratePayroll.js
@@ -1,28 +1,29 @@
 import React, { Component } from 'react';
 import { Panel, Alert, Button, FormGroup, ControlLabel, HelpBlock } from 'react-bootstrap';
-import DayPickerInput from 'react-day-picker/DayPickerInput';
-import * as moment from 'moment';
 
-import 'react-day-picker/lib/style.css';
+import DatePicker from 'react-datepicker';
+import * as moment from 'moment'; 
+
+import 'react-datepicker/dist/react-datepicker.css';
 import './GeneratePayroll.css';
 
 class GeneratePayroll extends Component {
     state = {
-        startDate: moment().subtract(7, 'days').format('L'),
-        endDate: moment().subtract(1, 'days').format('L'),
+        startDate: moment().subtract(7, 'days'),
+        endDate: moment().subtract(1, 'days'),
         error: ''
     }
 
     handleSubmit = (formSubmitEvent) => {
         formSubmitEvent.preventDefault();
-        console.log(formSubmitEvent);
+        console.log(this.state);
     }
 
     FieldGroup = ({ id, label, help, ...props }) => {
         return (
             <FormGroup controlId={id}>
                 <ControlLabel>{label}</ControlLabel>
-                <DayPickerInput { ...props} className={`form-control ${props.className}`} />
+                <DatePicker {...props} />
                 {help && <HelpBlock>{help}</HelpBlock>}
             </FormGroup>
         );
@@ -33,15 +34,23 @@ class GeneratePayroll extends Component {
             <Panel header="Generate Payroll">
                 {this.state.error && <Alert bsStyle="danger">{this.state.error}</Alert>}
                 <form onSubmit={this.handleSubmit}>
-                    <this.FieldGroup className="generate-date-selector" id="startDate" value={this.state.startDate} label="Start Date: " />
-                    <this.FieldGroup className="generate-date-selector" id="endDate" value={this.state.endDate} label="End Date: " />
+                    <this.FieldGroup 
+                        id="startDate" 
+                        label="Start Date" 
+                        selected={this.state.startDate} 
+                        onChange={(date)=>(this.setState({startDate: date}))} />
+                    <this.FieldGroup 
+                        id="endDate"
+                        label="End Date"
+                        selected={this.state.endDate}
+                        onChange={(date)=>(this.setState({endDate: date}))} />
                     <Button bsStyle="primary" type="submit"
                         disabled={this.state.items &&
                             this.state.items.every((e) => (!e.checked))}>
                         {'Generate'}
                     </Button>
                 </form>
-            </Panel>
+            </Panel >
         );
     }
 }
